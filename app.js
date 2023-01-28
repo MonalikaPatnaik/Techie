bd7fa92b111b281a8a02584c0f90f5c9d505d120
Monalika Patnaik <monalikapatnaik114@gmail.com>
2023-01-28 20:31:10 +0530

    added-buttons

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const axios=require('axios')
 const bot = new Telegraf(`5835127916:AAEVJEhZOKpcikc_kXSNOKgxAq55EVF8rek`)
 const web_link="https://castor23.netlify.app/#home"
 bot.start((ctx) => 
-ctx.reply("Hey there👋, tech-savvy students!\n\nAre you ready to put your creative minds to the test and show off your innovative ideas👀? Meet<b> Techie 😎</b>, your friendly bot guide to the upcoming <b>Castor Hackathon!</b> With Techie by your side, you'll have access to all the resources and support you need to create something truly amazing.🤯\nSo what are you waiting for? Let's go and register to convert your ideas into reality and make your mark in the tech world!👩‍💻\n\nType <b>/Menu </b>to explore what I have in store for you :))",{parse_mode:"HTML"},{
+ctx.reply("Hey there👋, tech-savvy students!\n\nAre you ready to put your creative minds to the test and show off your innovative ideas👀? Meet<b> Techie 😎</b>, your friendly bot guide to the upcoming <b>Castor Hackathon!</b> With Techie by your side, you'll have access to all the resources and support you need to create something truly amazing.🤯\nSo what are you waiting for? Let's go and register to convert your ideas into reality and make your mark in the tech world!👩‍💻\n\nType <b>/Menu </b>to explore what I have in store for you :))",{
     reply_markup:{inline_keyboard:[[
         {text:"Explore Website",url:web_link}]
-    ]}
+    ]},parse_mode:"HTML"
 }))     
 bot.command('Menu',(ctx)=>{
     // url= "https://castor23.netlify.app/"
